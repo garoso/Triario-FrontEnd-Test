1190c43b49d8eff6460aeabe96ff00ea5acb8d6a
Davste <david.fd.251@gmail.com>
2021-02-28 18:31:24 -0500

    Configuro la petición http para mostrarla en un componente ul sencillo de react

--- penguins/src/App.js
+++ penguins/src/App.js
@@ -1,9 +1,39 @@
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      penguins: []
+    };
+
+    axios.get("https://es.wikipedia.org/w/api.php?action=query&list=search&srprop=snippet&exlimit=5&format=json&origin=*&utf8=&srsearch=Ping%C3%BCinos_de_ficci%C3%B3n")
+      .then(response => {
+        this.setState({
+          penguins: response.data.query.search
+        })
+      })
+      .catch( error => {
+        console.log(error);
+      })
+  }
+
+render() {
   return (
-    <h1>Penguins</h1>
-  );
+    <div>
+      <ul>
+      <h1>Penguins</h1>
+        {this.state.penguins.slice(0,5).map(penguin =>
+          <li key={penguin.title}>{penguin.title}</li>
+        )}
+      </ul>
+    </div>
+
+    );
+  }
 }
 
 export default App;
