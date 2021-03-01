7c100ac2dc9ba2562f9541db1213396a6edd9c03
Davste <david.fd.251@gmail.com>
2021-03-01 00:41:45 -0500

    Realizo la busqueda del pokemon en el api con el nombre al hacer submit en el formulario

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -1,11 +1,39 @@
+import { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>POKEMON</h1>
-    </div>
-  );
+
+class SearchPokemon extends Component {
+  state = {
+    searchPokemon: '',
+  }
+
+  onChange = (event) =>{
+    this.setState({searchPokemon:event.target.value})
+  }
+
+  onSubmit = (event) =>{
+    event.preventDefault();
+    this.getPokemon(this.state.searchPokemon)
+  }
+
+  getPokemon (pokemon){
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+      .then((response) => {
+        console.log(response.data)
+      })
+  }
+  render () {
+    return(
+      <div className="App">
+        <h1>POKEMON</h1>
+        <form onSubmit={this.onSubmit}>
+          <input type="text" onChange={this.onChange}/>
+        </form>
+      </div>
+
+    )
+  }
 }
 
-export default App;
+export default SearchPokemon;
